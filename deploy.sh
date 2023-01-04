bc55e32d4bf1cd890a96c11fe8757a2fed83837e
Sun Duo <1075079562@qq.com>
2023-01-04 11:18:32 +0800

    Site updated: 2023-01-04 11:18:31

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@ rm -rf .deploy_git
 echo -ne '\n' | ssh root@sundocker.online rm -rf /usr/local/nginx/www/blog/*
 mkdir .deploy_git
 cp -r source/.github .deploy_git/
-npm run deploy
+hexo clean
+hexo generate
+hexo deploy
 scp -r public/* root@sundocker.online:/usr/local/nginx/www/blog/
